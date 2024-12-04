c96cfd1514f1cc2df8a84c1c72a32470d56f6cb1
Pan_Janke <jan.krol.legowski@gmail.com>
2024-12-04 02:50:50 +0100

    druga proba

--- day2/day2p2.py
+++ day2/day2p2.py
@@ -1,14 +1,9 @@
 
-# ten testowy pokazuje dokladnie ze posralem przyogotwanie danych wejsciowych
-# bo operuje caly czas na stringu a nie zbiorze intow
-# niechce mi sie tego juz ruszac
-def is_report_safe(line):
-    report = line.split()
-    print(line)
+# prawidlowy wynik to 300 z hakiem
+def is_report_safe(report):
     isIncrease = None
 
     for i in range(len(report) - 1):
-        print(report[i])
         adj = int(report[i]) - int(report[i + 1])
         if abs(adj) > 3 or abs(adj) < 1:
             if isIncrease is None:
@@ -27,21 +22,24 @@ def is_report_safe(line):
 
 
 sum = 0
-with open('../inputs/day2test.txt', 'r') as file:
+with open('../inputs/day2.txt', 'r') as file:
     for line in file:
+        line=line.split()
         index = is_report_safe(line)
-        line.split()
+
         if index == -1:
             sum = sum + 1
-        elif index == False:
-            stringA = line[:0] + line[0 + 1:]
-            stringB = line[:1] + line[1 + 1:]
-            if is_report_safe(stringA) == -1 or is_report_safe(stringB) == -1:
-                sum = sum + 1
         else:
-            stringC = line[:index] + line[index + 1:]
-            if is_report_safe(stringC) == -1:
-                sum = sum + 1
+            if index == False:
+                stringA = line[:0] + line[0 + 1:]
+                stringB = line[:1] + line[1 + 1:]
+                if is_report_safe(stringA) == -1 or is_report_safe(stringB) == -1:
+                    sum = sum + 1
+            else:
+                stringC = line[:index] + line[index + 1:]
+                if is_report_safe(stringC) == -1:
+                    sum = sum + 1
+                    print(stringC)
 
 print(sum)
 
