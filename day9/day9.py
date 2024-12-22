b44aa30c76a48b232d9f54e7d31dc2e2287b42ad
Pan_Janke <jan.krol.legowski@gmail.com>
2024-12-22 19:49:39 +0100

    bad day

--- day9/day9.py
+++ day9/day9.py
@@ -4,7 +4,9 @@ def find_dots(text):
     i = 0
     while (i < len(text)):
         if text[i] == '.':
-            while (text[i + j] == '.'):
+            while (i + j < len(text)):
+                if text[i + j] != '.':
+                    break
                 j += 1
             result.append((i, j))
             i = i + j - 1
@@ -31,19 +33,27 @@ for i in range(0, length):
         for j in range(0, int(text[i])):
             memory += '.'
         is_file = True
+memory.rstrip('.')
 print(memory)
 
 gap_list = find_dots(memory)
 print(gap_list)
-print(len(memory))
-
 
 for gap in gap_list:
     print(gap)
     l = len(memory)
     index, size = gap
-    size = gap[1]
-    candidate = memory[l - size:l]
+    if index + size < l - size:
+        candidate = memory[l - size:l]
 
-    print(candidate)
+
+        candidate = candidate.replace('.', '')
+        reverse = candidate[::-1]
+        num = index+len(reverse)-1
+        memory = memory[0:l - size]
+        l=len(memory)
+        memory = memory[0:index] + reverse + memory[l-num:l]
+
+    print(memory)
+    print(reverse)
     break
